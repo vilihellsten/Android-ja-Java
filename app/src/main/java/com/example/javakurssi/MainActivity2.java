a5a154c7e62509e8aea5fe8a4889d9fff36fc037
vilihellsten <vilihellsten@hotmail.com>
2024-03-15 10:26:47 +0200

    15.3

--- app/src/main/java/com/example/javakurssi/MainActivity2.java
+++ app/src/main/java/com/example/javakurssi/MainActivity2.java
@@ -14,9 +14,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity2";
     private Button helloButton;
+    private Button startButton;
     private TextView helloView;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -27,8 +30,16 @@ public class MainActivity2 extends AppCompatActivity {
             return insets;
         });
 
+        helloView = (TextView) findViewById(R.id.helloView);
         helloButton = (Button) findViewById(R.id.helloButton);
         helloButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleOnClickEvents(v);
+            }
+        });
+
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 handleOnClickEvents(v);
             }
@@ -40,7 +51,16 @@ public class MainActivity2 extends AppCompatActivity {
         //if else kokeile?
         switch(v.getId()){
             case R.id.helloButton:
-                Log.d("MainActivity2", "User tapped the HelloWorld button");
+                Log.d(TAG, "User tapped the Hello button");
+                if(helloView.getVisibility() == View.GONE )
+                { //ei ole constraintteja helloViewvissä joten muut elementit eivät liiku, gone toimii
+                    helloView.setVisibility(View.VISIBLE);
+                } else {
+                    helloView.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.startButton:
+                Log.d(TAG, "User tapped the Start Game button");
 
                 break;
         }
